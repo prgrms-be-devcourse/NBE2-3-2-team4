4924ba45566b2da82501d3cf8ae93b39eb77eec1
HeoJunSeo <wnstj56465@gmail.com>
2024-12-17 22:48:37 +0900

    9-feat-PkltStatusRepository에 주차장 상태 조회 할 수 있는 메소드 추가

--- src/main/java/com/team4/ttukttak_parking/domain/pkltstatus/repository/PkltStatusRepository.java
+++ src/main/java/com/team4/ttukttak_parking/domain/pkltstatus/repository/PkltStatusRepository.java
@@ -3,6 +3,9 @@ package com.team4.ttukttak_parking.domain.pkltstatus.repository;
 import com.team4.ttukttak_parking.domain.pkltstatus.entity.PkltStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PkltStatusRepository extends JpaRepository<PkltStatus, Long> {
+import java.util.Optional;
 
+public interface PkltStatusRepository extends JpaRepository<PkltStatus, Long> {
+    //주차장 고유 아이디로 주차장 상태 조회 Optional로 타입을 지정해 기능 사용
+    Optional<PkltStatus> findByPkltStatusId(Long id);
 }
