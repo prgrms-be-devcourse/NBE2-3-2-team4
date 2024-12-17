490181e429129158aa20239fd0be2539680fbd22
myqewr <mygongjoo@gmail.com>
2024-12-17 15:33:06 +0900

    [feat]#13:좌표 근처 주차장 목록 조회

--- src/main/java/com/team4/ttukttak_parking/controller/PkltController.java
+++ src/main/java/com/team4/ttukttak_parking/controller/PkltController.java
@@ -2,18 +2,21 @@ package com.team4.ttukttak_parking.controller;
 
 import com.team4.ttukttak_parking.domain.pklt.dto.PkltInfoResponse;
 import com.team4.ttukttak_parking.domain.pklt.dto.PkltResponse;
+import com.team4.ttukttak_parking.domain.pklt.dto.PkltSimpleDto;
 import com.team4.ttukttak_parking.domain.pklt.service.PkltService;
 import com.team4.ttukttak_parking.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +34,17 @@ public class PkltController {
     @GetMapping("/{pkltId}")
     public ResponseEntity<ApiResponse<PkltResponse>> getParkingLots(@PathVariable Long pkltId) {
         return ResponseEntity.ok().body(ApiResponse.createSuccess(pkltService.getParkingLots(pkltId)));
+    }
+
+
+    @Operation(summary = "좌표 근처 주차장 정보 조회 API", description = "좌표 근처 주차장 정보를 조회합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공")
+    })
+    @GetMapping("/search")
+    public ResponseEntity<ApiResponse<List<PkltSimpleDto>>> getParkingLots(@Parameter(description = "위도") @RequestParam(value = "lat") BigDecimal lat, @Parameter(description = "경도") @RequestParam(value ="lng") BigDecimal lng ){
+        return ResponseEntity.ok().body(ApiResponse.createSuccess(pkltService.getCloseParkingLots(lat,lng)));
+
     }
 
     @GetMapping("/{pkltId}/info")
