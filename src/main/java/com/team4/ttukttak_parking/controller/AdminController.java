05f76de62b03a0c4ecad9705882305a45378a4a4
DoHyun <citycozyboy@gmail.com>
2024-12-17 12:40:12 +0900

    feat: Swagger UI 설정
    SwaggerConfig 클래스 생성 및 Controller 어노테이션 추가
    
    Swagger-ui/index.html 진입 경로를 "http://localhost:8080/test" 로 변경

--- src/main/java/com/team4/ttukttak_parking/controller/AdminController.java
+++ src/main/java/com/team4/ttukttak_parking/controller/AdminController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
