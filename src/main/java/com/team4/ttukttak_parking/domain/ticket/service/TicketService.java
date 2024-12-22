1d69b7643fe9f433b54e0540b09de8939d12c5f7
HeoJunSeo <wnstj56465@gmail.com>
2024-12-23 04:47:01 +0900

    feature/#55-임시저장

--- src/main/java/com/team4/ttukttak_parking/domain/ticket/service/TicketService.java
+++ src/main/java/com/team4/ttukttak_parking/domain/ticket/service/TicketService.java
@@ -8,6 +8,8 @@ import com.team4.ttukttak_parking.domain.ticket.entity.Ticket;
 import com.team4.ttukttak_parking.domain.ticket.repository.TicketRepository;
 import com.team4.ttukttak_parking.global.exception.ErrorCode;
 import com.team4.ttukttak_parking.global.exception.NotFoundException;
+
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -48,5 +50,48 @@ public class TicketService {
         }
 
         return null;
+    }
+
+    //주차권 생성
+    @Transactional
+    public void addPkltTicket(){
+        List<Pklt> pkltList = pkltRepository.findAll();
+        int[]nums={2, 3, 4, 6};
+        for (Pklt pklt : pkltList) {
+
+            PkltInfo pkltInfo = pkltInfoRepository.findByPklt(pklt)
+                    .orElseThrow(() -> new NotFoundException(ErrorCode.PKLT_NOT_FOUND));
+            //시작시간과 종료시간이 0인 데이터 검증 로직 예외처리
+            if((pkltInfo.getWdOperBgngTm()==0&&pkltInfo.getWdOperEndTm()==0)
+                    &&(pkltInfo.getWeOperBgngTm()==0&&pkltInfo.getWeOperEndTm()==0)){
+
+                throw new NotFoundException(ErrorCode.PKLT_NOT_DATA_FOUND);
+
+            }
+
+            for (int num : nums) {
+                int bscPrkCrg = pkltInfo.getBscPrkCrg();
+                int bscPrkHr = pkltInfo.getBscPrkHr();
+
+                if (bscPrkCrg == 0) {
+                    bscPrkCrg = 260;
+                    bscPrkHr = 5;
+                }
+                if(bscPrkHr!=5) {  //분위당 요금이5가아닌 주차장 5로 통일
+
+                    bscPrkHr = 5;
+                }
+
+                int result = (bscPrkCrg / bscPrkHr ) * 60 * num;
+                ticketRepository.save(Ticket.to(pklt, result, num));
+            }
+
+            boolean isPklt24Hours=pkltInfo.getW
+        }
+
+
+        }
+
+
     }
 }
