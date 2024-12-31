8270fd9e084fb094c06736ff96369349a4744e8d
citycozy <citycozyboy@gmail.com>
2024-12-31 21:44:18 +0900

    fix

--- src/main/java/com/team4/ttukttak_parking/config/WebConfig.java
+++ src/main/java/com/team4/ttukttak_parking/config/WebConfig.java
@@ -22,8 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedOriginPatterns("http://localhost:3000")
             .allowedOriginPatterns(allowOrigins)
             .allowedHeaders("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
